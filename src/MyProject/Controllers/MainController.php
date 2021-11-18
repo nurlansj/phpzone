6a4aeefde6159aaa72f95a50c389797f3cc940b9
unknown <nurlan_sj@mail.ru>
2021-11-18 13:28:33 +0500

    test-branch1 commit1

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -12,7 +12,7 @@ class MainController extends ParentController
         ];
         $this->view->renderHtml('main.php', ['articles' => $articles]);
     }
-    public function sayHello(string $name)
+    public function sayHello(string $name1)
     {
         $this->view->renderHtml('hello.php', ['name' => $name]);
     }
