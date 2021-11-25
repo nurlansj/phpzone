a0a38cf29d56116d2dc12d780d5b4a3161eed149
unknown <nurlan_sj@mail.ru>
2021-11-25 21:39:06 +0500

    branch-master lesson16 home work adding author name

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,4 +1,5 @@
 <?php include __DIR__ . '/../header.php'; ?>
+    <h4><?= $author['nickname'] ?></h4>
     <h2><?= $article['name'] ?></h2>
     <p><?= $article['text'] ?></p>
 <?php include __DIR__ . '/../footer.php';  ?>
