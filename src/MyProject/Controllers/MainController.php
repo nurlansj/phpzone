722040f7e93f046264d0d3c4787d9feb20196cc7
unknown <nurlan_sj@mail.ru>
2021-11-25 12:50:57 +0500

    master-branch lesson15

--- src/MyProject/Controllers/MainController.php
+++ src/MyProject/Controllers/MainController.php
@@ -1,18 +1,24 @@
 <?php
 namespace MyProject\Controllers;
 use Vendor\Controllers\ParentController;
+use MyProject\Services\Db;
 
 class MainController extends ParentController
 {   
-    
-    //test-branch
+    private $db;
+    public function __construct() {
+        parent::__construct();
+        $this->db = new Db();
+    }
     public function main()
     {
-        $articles = [
-            ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
-            ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
-        ];
-        $this->view->renderHtml('main.php', ['articles' => $articles]);
+        $articles = $this->db->query('SELECT * FROM `articles`;');
+        var_dump($articles);
+        // $articles = [
+        //     ['name' => 'Статья 1', 'text' => 'Текст статьи 1'],
+        //     ['name' => 'Статья 2', 'text' => 'Текст статьи 2'],
+        // ];
+        // $this->view->renderHtml('main.php', ['articles' => $articles]);
     }
     public function sayHello(string $name)
     {
---
+++ src/MyProject/Services/Db.php
@@ -0,0 +1,25 @@
+<?php
+namespace MyProject\Services;
+
+class Db
+{
+    private $pdo;
+    public function __construct() {
+        $dbOptions = (require __DIR__ . '/../../settings.php')['db'];
+        $this->pdo = new \PDO(
+            'mysql:host=' . $dbOptions['host'] . ';dbname=' . $dbOptions['dbname'],
+            $dbOptions['root'],
+            $dbOptions['password']
+        );
+        $this->pdo->exec('SET NAMES UTF8');
+    }
+
+    public function query(string $sql, $params = []): ?array {
+        $sth = $this->pdo->prepare($sql);
+        $result = $sth->execute($params);
+        if ($result === false) {
+            return null;
+        }
+        return $sth->fetchAll();
+    }
+}
