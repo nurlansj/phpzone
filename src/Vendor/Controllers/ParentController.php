7656321ce0d9c0b056f0d79353ecf9b03cfceac5
unknown <nurlan_sj@mail.ru>
2021-11-27 14:44:04 +0500

    branch-master lesson19 singleton, changing in ParentController without reflection

--- src/Vendor/Controllers/ParentController.php
+++ src/Vendor/Controllers/ParentController.php
@@ -7,8 +7,15 @@ abstract class ParentController
     protected $view;
     public function __construct() {
         $className = static::class;
-        $reflector = new \ReflectionClass($className);
-        $viewFolder = lcfirst(substr($reflector->getShortName(),0,-10));
+        $pieces = explode('\\', $className);
+        $shortName = array_pop($pieces);
+        $viewFolder = lcfirst(substr($shortName,0,-10));
         $this->view = new View(__DIR__ . '/../../../templates/' . $viewFolder);
+
+        // get shortName of static Class using \ReflectionClass
+        // return;
+        // $reflector = new \ReflectionClass($className);
+        // $viewFolder = lcfirst(substr($reflector->getShortName(),0,-10));
+        // $this->view = new View(__DIR__ . '/../../../templates/' . $viewFolder);
     }
 }
